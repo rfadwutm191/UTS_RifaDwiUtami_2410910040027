f29f66d4043ab438755c0982e4de9b9c48925e9a
rfadwutm <rifautami191@gmail.com>
2025-10-23 19:15:32 +0700

    update

--- lib/uts/login.dart
+++ lib/uts/login.dart
@@ -5,6 +5,14 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          
+        ),
+      ),
+
+    );
   }
 }
