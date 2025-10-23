12169e317887cb2c29d3fc78584329c0103ae94d
rfadwutm <rifautami191@gmail.com>
2025-10-23 21:44:57 +0700

    update daftar

--- lib/uts/daftar.dart
+++ lib/uts/daftar.dart
@@ -8,20 +8,24 @@ class DaftarPage extends StatefulWidget {
 }
 
 class _DaftarPageState extends State<DaftarPage> {
+  bool _showPassword = true;
+  bool _showKonfirmasi = true;
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _alamatController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
+          // GANTI SingleChildScrollView jadi ListView
           children: [
             SizedBox(height: 60),
             Center(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset('gambar/logo.png', height: 150),
+                  Image.asset('gambar/logo.png', height: 100),
                   SizedBox(height: 8),
                   Text(
                     "LKS MART",
@@ -31,28 +35,53 @@ class _DaftarPageState extends State<DaftarPage> {
                       color: Colors.blue,
                     ),
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        "Daftar",
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-
-                      SizedBox(height: 8),
-                      Column(
-                        children: [
-                          Text("Masukkan Data Pribadi Anda")
-                        ],
-                      )
-                    ],
-                  ),
                 ],
               ),
             ),
+            SizedBox(height: 30),
+            Text(
+              "Daftar",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 4),
+            Text(
+              "Silahkan isi Data Pribadi Anda",
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(height: 20),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nama Lengkap',
+                hintText: 'Nama Lengkap',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Alamat',
+                hintText: 'Alamat',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                hintText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
           ],
         ),
       ),
